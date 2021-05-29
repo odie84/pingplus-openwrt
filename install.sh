ca697a9e3a3e8ec0833a5308c604e80575f8c32c
Sachnun Kusmayadi <sachnun12@gmail.com>
2021-05-29 15:35:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 # Download and save
-echo -e "Mengunduh pingplus!"
+echo "Mengunduh pingplus!"
 curl -sLko "/root/pingplus.sh" "https://github.com/sachnun/pingplus-openwrt/raw/main/bash/pingplus.sh"
 chmod +x "/root/pingplus.sh"
 
 # Set Autostart
-echo -e "Konfigurasi pingplus autostart!"
+echo "Konfigurasi pingplus autostart!"
 sed -i "/pingplus.sh/d" /etc/rc.local
 sed -i "s/exit 0/$(echo "screen -AmdS pingplus /root/pingplus.sh" | sed 's/\//\\\//g')\nexit 0/g" /etc/rc.local
 
 # Running
-echo -e "Menjalankan pingplus!"
+echo "Menjalankan pingplus!"
 screen -AmdS pingplus /root/pingplus.sh
 
-echo -e "" && echo -e "Berhasil." && exit
+echo "" && echo "Berhasil." && exit
